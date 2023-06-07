7c47b161f1e30a81e8a2269e8afd7dcba1475c56
alekslesik <alekslesik@gmail.com>
2023-06-07 14:20:14 +0300

    add include files for catalog

---
+++ include/include/catalog/information/calculation.php
@@ -0,0 +1,10 @@
+<div class="catalog-information">
+    <div class="catalog-information-items">
+        <div class="catalog-information-item">
+            <div class="catalog-information-item-title">
+                Светотехнический расчет
+            </div>
+            <div class="catalog-information-item-description">
+              Illi enim inter se dissentiunt. Aliter enim explicari, quod quaeritur, non potest. Compensabatur, inquit, cum summis doloribus laetitia. Inde igitur, inquit, ordiendum est. Res enim concurrent contrariae. Neutrum vero, inquit ille. Ita prorsus, inquam; 
+        </div>
+</div>
